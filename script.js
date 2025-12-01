ecedf464a966b8d726737103df673dc3dbf63708
hasithbasnayake <hasith@ucsb.edu>
2025-11-30 18:59:07 -0800

    Cleaned up codebase

--- script.js
+++
@@ -1,7 +0,0 @@
-const chunks = document.querySelectorAll('main > *');
-
-chunks.forEach((el, i) => {
-  el.style.opacity = 0; // start hidden
-  el.style.animation = `fadeInLeft 0.8s forwards`;
-  el.style.animationDelay = `${i * .15}s`; // stagger
-});
